2b6c2e7d5e107f4c0d4b986d00dda5402afbfe5e
George dos Santos <fredweb@globo.com>
2015-11-14 14:18:02 -0300

    Versão FInal

--- app/src/main/java/com/example/george/materialdesign/deserialize/CharacterDeserializer.java
+++ app/src/main/java/com/example/george/materialdesign/deserialize/CharacterDeserializer.java
@@ -7,8 +7,11 @@ import com.google.gson.JsonDeserializationContext;
 import com.google.gson.JsonDeserializer;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonParseException;
+import com.google.gson.reflect.TypeToken;
 
 import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by george on 04/11/15.
@@ -18,11 +21,17 @@ public class CharacterDeserializer implements JsonDeserializer<Object> {
     public Object deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context) throws JsonParseException {
 
         JsonElement character = json.getAsJsonObject();
+        DataCharacter retorno = new DataCharacter();
+
         if(json.getAsJsonObject().get("data") != null)
         {
             character =json.getAsJsonObject().get("data");
+            if(character.getAsJsonObject().get("results")!=null)
+            {
+                Type type = new TypeToken<ArrayList<Character>>() {}.getType();
+                return  new Gson().fromJson(character.getAsJsonObject().get("results"), type);
+            }
         }
-        DataCharacter retorno = new Gson().fromJson(character, DataCharacter.class);
         return (retorno );
     }
 }
